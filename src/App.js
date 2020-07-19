8f0b1ff0ae976981e9ade77a76a4df29d0e25303
melissakez <kezssameli@gmail.com>
2020-07-19 09:06:47 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import axios from "axios";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    ticker: {
+      buy: [],
+      sell: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://indodax.com/api/btc_idr/depth")
+      .then((result) => {
+        this.setState({
+          ticker: {
+            buy: result.data.buy,
+            sell: result.data.sell
+          }
+        })
+      });
+  }
+
+  render() {
+    function thousandSeparator(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
+
+    const buyData = this.state.ticker.buy.map((val)=>{
+      return <tr>
+        <td>{thousandSeparator(val[0])}</td>
+        <td>{val[1]}</td>
+        <td>{thousandSeparator(Math.floor(val[0] * parseFloat(val[1])))}</td>
+      </tr>;
+     })
+
+     const sellData = this.state.ticker.sell.map((val)=>{
+      return <tr>
+        <td>{thousandSeparator(val[0])}</td>
+        <td>{val[1]}</td>
+        <td>{thousandSeparator(Math.floor(val[0] * parseFloat(val[1])))}</td>
+      </tr>;
+     })
+    
+    return (
+      <div className="tables">
+        <div className="market sellmarket">
+          <h3>Sell Market</h3>
+          <table border="1">
+            <tr>
+              <th>Price</th>
+              <th>BTC</th>
+              <th>IDR</th>
+            </tr>
+            {sellData}
+          </table>
+        </div>
+        <div className="market buymarket">
+          <h3>Buy Market</h3>
+          <table border="1">
+            <tr>
+              <th>Price</th>
+              <th>BTC</th>
+              <th>IDR</th>
+            </tr>
+            {buyData}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
